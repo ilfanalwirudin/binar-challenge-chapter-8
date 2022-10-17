835c4c41f7df6ce7bd93bda90e93c5f32f88cfad
Ilfan Alwirudin <muhammadilfan7@gmail.com>
2022-10-18 00:03:10 +0700

    fix client not detected

---
+++ react-client/src/components/Footer.js
@@ -0,0 +1,13 @@
+const Footer = () => (
+  <footer className="bg-dark text-light text-center py-2">
+    <p className="m-0">
+      Ngoding with{" "}
+      <span role="img" aria-label="love">
+        ♥️
+      </span>{" "}
+      by <span>Kang Sami</span>
+    </p>
+  </footer>
+);
+
+export default Footer;
